862d4f96a926aecc07e5310f17e902ab07d29b1f
Dongphamvanbkap <51324401+Dongphamvanbkap@users.noreply.github.com>
2021-01-07 23:27:25 +0700

    Add files via upload

---
+++ change_color_theme/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Change Color Theme",
+    'summary': """Change Color Theme""",
+    'description': """
+        Change Color Theme
+    """,
+    'author': "We can Odoo it",
+    'sequence': 290,
+    'website': "",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'web'],
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'active': True,
+}
